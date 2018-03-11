a4753b3e353b3e0aef30c6e118c6b5523d0242ca
= <webdev.jameslemire@gmail.com>
2018-03-11 14:20:24 -0600

    JL master - Fix index.js and render the AddPerson component.

---
+++ public/src/components/AddPerson/AddPerson.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './AddPerson.css'
+
+export default class AddPerson extends Component {
+   render () {
+      return (
+         <div>
+            <span>Add Person Component Here</span>
+         </div>
+      )
+   }
+}
--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Components
+import AddPerson from './AddPerson/AddPerson'
+
 class App extends Component {
    render() {
       return (
          <div className="App">
-
+            <AddPerson />
          </div>
       );
    }
