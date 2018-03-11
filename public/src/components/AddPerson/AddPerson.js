f3f5548ba82c844460316e76c6e84e9e167e65d5
= <webdev.jameslemire@gmail.com>
2018-03-11 14:33:33 -0600

    JL master - Add elements to make a person in the AddPerson component.

--- public/src/components/AddPerson/AddPerson.js
+++ public/src/components/AddPerson/AddPerson.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react'
 import './AddPerson.css'
 
 export default class AddPerson extends Component {
+   constructor () {
+      super()
+      this.state = {
+         name: '',
+         height: '',
+         age: '',
+         ss_num: '',
+         gender: 'female',
+         hasTattoos: false
+      }
+   }
+
    render () {
       return (
          <div>
-            <span>Add Person Component Here</span>
+            <input placeholder="Name" />
+            <input placeholder="Height" />
+            <input placeholder="Age" />
+            <input placeholder="Social Number" />
+            <div>
+               <span>Gender</span>
+               <select>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+               </select>
+            </div>
+            <div>
+               <span>Has Tattoos</span>
+               <select>
+                  <option value={ true }>Yes</option>
+                  <option value={ false }>No</option>
+               </select>
+            </div>
          </div>
       )
    }
