75dc4e45fbb03b9db5de93e339fbaf3049cc0f81
= <webdev.jameslemire@gmail.com>
2018-03-11 15:18:54 -0600

    JL master - Working get request with fake data.

--- controllers/person.js
+++ controllers/person.js
@@ -1,8 +1,13 @@
-let data = []
+let data = [
+   'string',
+   'string',
+   5,
+   true
+]
 
 module.exports = {
    get: ( req, res ) => {
-
+      res.status(200).send(data)
    },
    put: ( req, res ) => {
 
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const personController = require('./controllers/person')
 
 const app = express()
+
+app.get('/api/people', personController.get)
 
 const port = 3001
 app.listen(port, () => console.log(`Server listening on port ${port}`))
