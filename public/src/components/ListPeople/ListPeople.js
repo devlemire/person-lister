5496169e668887355256d1ede2faa181f8718939
= <webdev.jameslemire@gmail.com>
2018-03-11 16:46:11 -0600

    JL master - Add template for ListPeople and Person components.

---
+++ public/src/components/ListPeople/ListPeople.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListPeople extends Component {
+   render () {
+      return (
+         <div>
+            <span>ListPeople component here</span>
+         </div>
+      )
+   }
+}
---
+++ public/src/components/ListPeople/Person/Person.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Person extends Component {
+
+
+   render () {
+      return (
+         <div>
+            <span>Person Component Here</span>
+         </div>
+      )
+   }
+}
